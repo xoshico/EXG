4bbec42effc770e2332e53b3b0e233679c3f5392
Xoshico <ccgghhj124@gmail.com>
2024-03-04 19:53:20 +0330

    add canvas to html

--- main.js
+++ main.js
@@ -2,5 +2,16 @@ let app = null;
 
 document.addEventListener('DOMContentLoaded', function() {
     app = document.getElementById('app');
+
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width', '320');
+    canvas.setAttribute('height', '400');
+    canvas.setAttribute('style', 'border: 1px solid black');
+
+    const ctx = canvas.getContext('2d');
     
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(10, 10, 50, 50);
+
+    app.appendChild(canvas);
 });
